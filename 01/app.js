f88340dbe771f1e3c527b9cbb7d6dac4ed152556
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2020-03-28 22:18:33 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,39 @@
+console.log('app.js');
+
+//var count = 5;
+//var nick = "my nick is 'fe'";
+//var homepage = 'www.kakaocorp.com';
+
+//var isMan = true;
+
+//console.log(nick == '');
+
+//console.log(!!'test');
+/*
+var log = function(str) {
+    console.log(str);
+  };
+  function log(str) {
+    console.log(str);
+  }
+  */
+
+//var image = {};
+
+//image.width = 1;
+//image.height = 2;
+
+//image['width'] = 1;
+
+//image['name' + i] = '1.jpg'; 
+
+//image = { width: 1, height: 2 };
+
+//image = [];
+
+//image[0] = '1.jpg';
+//image[1] = '2.jpg';
+
+//image = ['1.jpg', 2, { name: '3.jpg' }];
+
+//var nick = null;
