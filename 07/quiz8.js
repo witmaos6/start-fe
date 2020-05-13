41ceab6423f7a673720d5c8fb7624e2f954e2a38
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2020-05-13 22:50:45 +0900

    07 과제ㅔ

---
+++ 07/quiz8.js
@@ -0,0 +1,34 @@
+const $box = document.getElementById('box');
+const $btn = document.getElementById('btn');
+const $min = document.getElementById('min');
+const $max = document.getElementById('max');
+
+let num = 0;
+
+function Random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function animation(result, btn, val) {
+  let min = val - 50;
+  const max = val;
+  const interval = setInterval(() => {
+    min += 1;
+    result.innerHTML = String(min);
+    if (min === max) {
+      btn.disabled = false;
+      clearInterval(interval);
+    }
+  }, 10);
+}
+
+function PrintNum() {
+  num = Math.floor(random($min.value, $max.value));
+}
+
+window.onload = function () {
+  $btn.addEventListener('click', () => {
+    $btn.disabled = true;
+    this.animation($box, btn, Random($min.value, $max.value));
+  });
+};
