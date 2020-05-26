dfbed49845effc0cddb87f573bebeb5cd0ba9970
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2020-05-26 17:51:41 +0900

    09 실습

---
+++ 09/kakao.js
@@ -0,0 +1,28 @@
+const $btn = document.querySelector("#search-btn");
+const $text = document.querySelector("#search-text");
+const url = 'https://dapi.kakao.com/v2/search/web';
+const headers = {
+    Authorization: "KakaoAK ee5adcecd697e1c53a34a89a7ef72589",
+};
+
+function success(data) {
+    console.log(data);
+}
+function error() {
+    console.log(2);
+}
+
+function search() {
+    const { value } = $text;
+    if (value === '') {
+        alert('검색어 입력');
+        return false;
+    }
+
+
+    fetch('${url}?query=${value}', { headers }).then((res) => res.json()).then(success).catch(error);
+
+    return true;
+}
+
+$btn.addEventListener('click', search);
